91832f69d3f5108dc96d92f3e98568b132a724bb
developer-BOGUM <bogumk1206@gmail.com>
2022-03-17 16:16:33 +0900

    관리자 페이지 퍼블리싱

--- src/main/resources/static/assets/js/admCommon.js
+++ src/main/resources/static/assets/js/admCommon.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
-
+    headerMenuControl();
 });
+
+//메뉴를 컨트롤한다
+function headerMenuControl() {
+    $('.header-list > ul > li').click(function(){
+        if($(this).hasClass('on')) {
+            $(this).addClass("off");
+            $(this).removeClass("on");
+        }
+        else if($(this).hasClass('off')) {
+            $(this).addClass("on");
+            $(this).removeClass("off");
+        }
+        // else {
+        //     location.href=$(this).data('val');
+        // }
+    });
+}
 
