00c19fdd71df440a38c8d9c116d535a2abfc7f09
ohjihoon2 <38777474+ohjihoon2@users.noreply.github.com>
2022-07-12 11:06:48 +0900

    싱글톤 CSS 추가 작업

--- src/main/java/com/example/demo/config/CssInterceptor.java
+++ src/main/java/com/example/demo/config/CssInterceptor.java
@@ -1,5 +1,7 @@
 package com.example.demo.config;
 
+import com.example.demo.vo.Css;
+import com.example.demo.vo.SingletonData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Slf4j
 public class CssInterceptor implements HandlerInterceptor {
@@ -16,31 +19,55 @@ public class CssInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("AlertInterceptor.postHandle");
 
         String requestURI = request.getRequestURI();//프로젝트경로부터 파일까지의 경로값을 얻어옴 (/test/index.jsp)
         requestURI = requestURI.replaceFirst("/","");
 
-        String[] split = requestURI.split("/");
-        String cssStyle = null;
+        String[] urlSplit = requestURI.split("/");
 
-        if(split[0].equals("")){
-            System.out.println("main 이다");
-        }else{
-            System.out.println("main 아니다");
-            if(split.length == 1){
-                System.out.println("2번째 없다");
+        List<Css> cssList = SingletonData.getInstance().getCssList();
+        StringBuffer cssStyle = new StringBuffer();
+
+        if(urlSplit[0].equals("")){
+            for (Css css : cssList) {
+                // main, common은 lvl2가 없다는 가정하에 진행
+                if(css.getCssFirstId().equals("main") || css.getCssFirstId().equals("common")){
+                    cssStyle.append("\n/* ------------------------------");
+                    cssStyle.append(css.getCssNm());
+                    cssStyle.append("------------------------------ */\n");
+                    cssStyle.append(css.getCssCode()+"\n");
+                }
+            }
+        }else {
+            if(urlSplit[0].equals("board")) {
+                for (Css css : cssList) {
+                    if(css.getCssFirstId().equals("common") || (css.getCssFirstId().equals("board"))){
+                        cssStyle.append("\n/* ------------------------------");
+                        cssStyle.append(css.getCssNm());
+                        cssStyle.append("------------------------------ */\n");
+                        cssStyle.append(css.getCssCode()+"\n");
+                    }
+                }
+            }else if(urlSplit[0].equals("content")){
+                for (Css css : cssList) {
+                    if(css.getCssFirstId().equals("common") || css.getCssFirstId().equals("content")){
+                        cssStyle.append("\n/* ------------------------------");
+                        cssStyle.append(css.getCssNm());
+                        cssStyle.append("------------------------------ */\n");
+                        cssStyle.append(css.getCssCode()+"\n");
+                    }
+                }
             }else {
-                System.out.println("2번째 있다 "+ split[1]);
+                for (Css css : cssList) {
+                    if(css.getCssFirstId().equals("common") || css.getCssFirstId().equals(urlSplit[0]))
+                    cssStyle.append("\n/* ------------------------------");
+                    cssStyle.append(css.getCssNm());
+                    cssStyle.append("------------------------------ */\n");
+                    cssStyle.append(css.getCssCode()+"\n");
+                }
             }
         }
-//
-//        if(session.getAttribute("idx") != null){
-//            userIdx = Integer.parseInt(String.valueOf(session.getAttribute("idx")));
-////            alertCnt = alarmService.countReadYn(userIdx);
-//            log.debug("alertCnt : {}", alertCnt);
-//
-//        }
+
         modelAndView.addObject("cssStyle",cssStyle);
         HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
     }
